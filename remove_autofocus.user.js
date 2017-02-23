9a52778a481c657be36e80388d8b6fd028b6043e
Michael Pande <michaelpande@gmail.com>
2017-02-23 14:09:11 +0100

    Initial commit

---
+++ remove_autofocus.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name Remove autofocus
+// @author Michael Pande <github.com/michaelpande>
+// @description Makes vim keybindings work correctly on webpages with autofocus
+// @version 1.0.0
+// @grant none
+// ==/UserScript==
+(function(){
+  document.addEventListener("DOMContentLoaded", function(){
+    var lostFocus = false;
+    var time = 3000;
+    var interval = 25;
+
+
+    function focusRemoval(){
+      time = time - interval;
+      if(lostFocus || time <= 0){
+        clearInterval(focusCheck);
+        return;
+      }
+      if(document.activeElement) {
+        lostFocus = true;
+        document.activeElement.blur();
+      }
+
+    }
+    var focusCheck = setInterval(focusRemoval, interval);
+  });
+})();
